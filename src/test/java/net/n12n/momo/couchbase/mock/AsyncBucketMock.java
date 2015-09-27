d1ad1b5c50528fa5aacc766a1136c43d9f900de8
Niklas Grossmann <ngrossmann@gmx.net>
2015-09-27 08:50:09 +0200

    Add license information to source files

--- src/test/java/net/n12n/momo/couchbase/mock/AsyncBucketMock.java
+++ src/test/java/net/n12n/momo/couchbase/mock/AsyncBucketMock.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2015 Niklas Grossmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.n12n.momo.couchbase.mock;
 
 import com.couchbase.client.core.ClusterFacade;
 import com.couchbase.client.deps.io.netty.buffer.ByteBuf;
-import com.couchbase.client.deps.io.netty.buffer.UnpooledHeapByteBuf;
 import com.couchbase.client.java.AsyncBucket;
 import com.couchbase.client.java.PersistTo;
 import com.couchbase.client.java.ReplicaMode;
